dfd626bda3b84b82b01460e596a857cbaf01418c
AmandaBarros94 <mandy.bjj1@gmail.com>
2022-09-21 12:03:02 -0300

    removendo status da propiedade

--- src/Middlewares/CarValidator.ts
+++ src/Middlewares/CarValidator.ts
@@ -10,8 +10,7 @@ const productSchema = Joi.object({
   model: Joi.string().min(3).required(),
   year: Joi.number().min(1900).max(2022),
   color: Joi.string().min(3).required(),
-  status: Joi.boolean(),
-  buyValue: Joi.number(),
+  buyValue: Joi.number().required(),
   doorsQty: Joi.number().min(2).max(4).required(),
   seatsQty: Joi.number().min(2).max(7).required(),
 });
