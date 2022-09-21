939c41886605bcf5c74fa0d427af9b4e32e6d717
AmandaBarros94 <mandy.bjj1@gmail.com>
2022-09-21 11:42:50 -0300

    completando o app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
+import 'express-async-errors';
 import express from 'express';
+import router from './routers';
+
+import handleErrors from './Middlewares/HandleError';
 
 const app = express();
+app.use(express.json());  
+app.use(router);
+app.use(handleErrors);
 
 export default app;
