cfd6b93b6a2c77df70d27485cf98a1b7804a1fb3
AmandaBarros94 <mandy.bjj1@gmail.com>
2022-09-21 22:43:56 -0300

    teste de cobertura

--- src/tests/unit/models/seu-teste-model.test.ts
+++ src/tests/unit/models/seu-teste-model.test.ts
@@ -1,22 +1,34 @@
-// template para criação dos testes de cobertura da camada de model
+import { expect } from 'chai';
+import sinon from 'sinon';
+import CarModel from '../../../models/Car';
+import { Model } from 'mongoose';
+import { car, carWithId } from '../../../tests/unit/mock/CarMock';
+
+describe('Testing Car Model', () => {
+	const carModel = new CarModel();
+
+	before(() => {
+		sinon.stub(Model, 'create').resolves(carWithId);
+		sinon.stub(Model, 'findOne').resolves(carWithId);
+	});
+
+	after(() => {
+		sinon.restore();
+	})
+
+	describe('If the creation of the car is a success', () => {
+		it('Expected on success', async () => {
+			const newCar = await carModel.create(car);
+			expect(newCar).to.be.deep.equal(carWithId);
+		});
+	});
+
+	describe('Searching for a car by id', () => {
+		it('Expected on success', async () => {
+			const carFound = await carModel.readOne('62cf1fc6498565d94eba52cd');
+			expect(carFound).to.be.deep.equal(carWithId);
+		});
+	});
 
 
-// import * as sinon from 'sinon';
-// import chai from 'chai';
-// const { expect } = chai;
-
-// describe('Sua descrição', () => {
-
-//   before(async () => {
-//     sinon
-//       .stub()
-//       .resolves();
-//   });
-
-//   after(()=>{
-//     sinon.restore();
-//   })
-
-//   it('', async () => {});
-
-// });
+});
