43f7c040cbda5e38abddac342beb87e6028436db
AmandaBarros94 <mandy.bjj1@gmail.com>
2022-09-21 22:32:03 -0300

    teste de cobertura

--- src/tests/unit/services/seu-teste-service.test.ts
+++ src/tests/unit/services/seu-teste-service.test.ts
@@ -1,22 +1,42 @@
-// template para criação dos testes de cobertura da camada de service
+import { expect } from 'chai';
+import * as sinon from 'sinon';
+import ErrorWithSatatus from '../../../Error';
+import CarModel from '../../../models/Car';
+import CarService from '../../../services/Car.service';
+import { carWithId, car } from '../../unit/mock/CarMock';
 
+describe('Testing Car Service', () => {
+  const carModel = new CarModel();
+  const carService = new CarService(carModel);
 
-// import * as sinon from 'sinon';
-// import chai from 'chai';
-// const { expect } = chai;
+  before(() => {
+    sinon.stub(carModel, 'create').resolves(carWithId);
+    sinon.stub(carModel, 'readOne').resolves(carWithId);
+  })
 
-// describe('Sua descrição', () => {
+  after(() => {
+    sinon.restore()
+  })
 
-//   before(async () => {
-//     sinon
-//       .stub()
-//       .resolves();
-//   });
+  describe('If the creation of the car is a success', () => {
+    it('Expected on success', async () => {
+      const carCreated = await carService.create(car);
+      expect(carCreated).to.be.deep.equal(carWithId);
+    });
 
-//   after(()=>{
-//     sinon.restore();
-//   })
+    it('Expected on failed', async () => {
+      try {
+        await carService.create({} as any);
+      } catch (error) {
+        expect(error).to.be.instanceOf(ErrorWithSatatus);
+      }
+    });
+  });
+  describe('Searching for a car by id', () => {
+    it('Expected on success', async () => {
+      const carFound = await carService.readOne('62cf1fc6498565d94eba52cd');
+      expect(carFound).to.be.deep.equal(carWithId);
+    });
 
-//   it('', async () => {});
-
-// });
+  });
+});
